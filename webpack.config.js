766a53cac0752d033ddc37cc79b988defe1e477d
jiang2016tao <824167117@qq.com>
2017-09-16 10:44:04 +0800

    配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,6 @@
-# webpack的安装
-   在安装的过程中，第一次使用npm install webpack，这样在当前目录执行webpack命令，发现报不是内部命令；使用npm install webpack -g就可以了。
-# webpack打包命令
-webpack app/index.js build/build.js
-# webpack配置文件
-``js
+/**
+ * Created by Administrator on 2017/9/16.
+ */
 const path = require('path');
 module.exports={
     entry:"./app/index.js",
@@ -22,4 +19,3 @@ module.exports={
         extensions:['js','css','jsx']
     }
 };
-``
