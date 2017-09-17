572b3454defc685ac6385862a3883a86f008c919
jiang2016tao <824167117@qq.com>
2017-09-17 21:58:04 +0800

    配置文件

---
+++ es6/build/build.js
@@ -0,0 +1,8 @@
+"use strict";
+
+/**
+ * Created by Administrator on 2017/9/17.
+ */
+var fn = function fn() {
+  console.log("test");
+};
---
+++ es6/test.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2017/9/17.
+ */
+var fn = () => {
+    console.log("test");
+};
